b60a3ba79b9f96c614080552ea063d7ef94e9236
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-12-27 19:52:35 +0530

    refix: db connection

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -5,10 +5,11 @@ dotenv.config();
 
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: { rejectUnauthorized: false },
   family: 4,
+  max: 5,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
 });
 
 export default db;
