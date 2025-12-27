0cd406963cc40e23b544a85dfdb7fc26cd6eb80d
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-12-27 19:32:11 +0530

    fix: database connection error

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,15 +1,14 @@
-import pkg from "pg";
+import { Pool } from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
-
-const { Pool } = pkg;
 
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
+  family: 4,
 });
 
 export default db;
